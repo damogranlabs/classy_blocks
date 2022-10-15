21c7c77b84fac2bfeab6559a684f6a02427237b1
schperplata <domen.jurkovic@gmail.com>
2022-10-15 18:42:36 +0200

    Reformated with black

--- src/classy_blocks/classes/walls.py
+++ src/classy_blocks/classes/walls.py
@@ -8,19 +8,26 @@ from classy_blocks.util import functions as f
 
 
 class ElbowWall(Shape):
-    inner_patch = 'left'
+    inner_patch = "left"
 
     sketch_class = Annulus
 
     def transform_function(self, **kwargs):
-        return self.sketch_1 \
-            .rotate(**kwargs) \
-            .scale(**kwargs)
+        return self.sketch_1.rotate(**kwargs).scale(**kwargs)
 
-    def __init__(self,
-                 center_point_1: List, outer_radius_point_1: List, normal_1: List, thickness_1: float,
-                 sweep_angle: float, arc_center: List, rotation_axis: List,
-                 outer_radius_2: float, thickness_2: float, n_segments: int = None):
+    def __init__(
+        self,
+        center_point_1: List,
+        outer_radius_point_1: List,
+        normal_1: List,
+        thickness_1: float,
+        sweep_angle: float,
+        arc_center: List,
+        rotation_axis: List,
+        outer_radius_2: float,
+        thickness_2: float,
+        n_segments: int = None,
+    ):
         # TODO: TEST
         self.arguments = locals()
 
@@ -31,19 +38,19 @@ class ElbowWall(Shape):
         super().__init__(
             [center_point_1, outer_radius_point_1, normal_1, inner_radius_1, n_segments],
             {
-                'axis': rotation_axis,
-                'angle': sweep_angle,
-                'origin': arc_center,
-                'outer_radius': outer_radius_2,
-                'inner_radius': inner_radius_2
+                "axis": rotation_axis,
+                "angle": sweep_angle,
+                "origin": arc_center,
+                "outer_radius": outer_radius_2,
+                "inner_radius": inner_radius_2,
             },
             {
-                'axis': rotation_axis,
-                'angle': sweep_angle / 2,
-                'origin': arc_center,
-                'outer_radius': (outer_radius_1 + outer_radius_2) / 2,
-                'inner_radius': (inner_radius_1 + inner_radius_2) / 2
-            }
+                "axis": rotation_axis,
+                "angle": sweep_angle / 2,
+                "origin": arc_center,
+                "outer_radius": (outer_radius_1 + outer_radius_2) / 2,
+                "inner_radius": (inner_radius_1 + inner_radius_2) / 2,
+            },
         )
 
         self.core = []
@@ -62,29 +69,41 @@ class ElbowWall(Shape):
             thickness_2 = thickness_1
 
         s1 = source.sketch_1
-        radius_point_1 = s1.center_point + \
-                         f.unit_vector(s1.radius_point - s1.center_point) * (s1.radius + thickness_1)
+        radius_point_1 = s1.center_point + f.unit_vector(s1.radius_point - s1.center_point) * (s1.radius + thickness_1)
 
         return cls(
-            source.sketch_1.center_point, radius_point_1, source.sketch_1.normal, thickness_1,
-            source.arguments['sweep_angle'], source.arguments['arc_center'], source.arguments['rotation_axis'],
-            source.sketch_2.radius + thickness_2, thickness_2, len(s1.shell_faces))
+            source.sketch_1.center_point,
+            radius_point_1,
+            source.sketch_1.normal,
+            thickness_1,
+            source.arguments["sweep_angle"],
+            source.arguments["arc_center"],
+            source.arguments["rotation_axis"],
+            source.sketch_2.radius + thickness_2,
+            thickness_2,
+            len(s1.shell_faces),
+        )
 
 
 class FrustumWall(Shape):
-    inner_patch = 'left'
+    inner_patch = "left"
 
     sketch_class = Annulus
 
     def transform_function(self, **kwargs):
-        return self.sketch_1 \
-            .translate(**kwargs) \
-            .scale(**kwargs)
+        return self.sketch_1.translate(**kwargs).scale(**kwargs)
 
-    def __init__(self, axis_point_1: List, axis_point_2: List,
-                 outer_radius_point_1: List, thickness_1: float,
-                 outer_radius_2: float, thickness_2: float,
-                 outer_radius_mid: float = None, n_segments=None):
+    def __init__(
+        self,
+        axis_point_1: List,
+        axis_point_2: List,
+        outer_radius_point_1: List,
+        thickness_1: float,
+        outer_radius_2: float,
+        thickness_2: float,
+        outer_radius_mid: float = None,
+        n_segments=None,
+    ):
 
         self.axis = np.asarray(axis_point_2) - np.asarray(axis_point_1)
 
@@ -96,19 +115,15 @@ class FrustumWall(Shape):
             mid_params = None
         else:
             mid_params = {
-                'vector': self.axis / 2,
-                'outer_radius': outer_radius_mid,
-                'inner_radius': outer_radius_mid - (thickness_1 + thickness_2) / 2
+                "vector": self.axis / 2,
+                "outer_radius": outer_radius_mid,
+                "inner_radius": outer_radius_mid - (thickness_1 + thickness_2) / 2,
             }
 
         super().__init__(
             [axis_point_1, outer_radius_point_1, self.axis, inner_radius_1, n_segments],
-            {
-                'vector': self.axis,
-                'inner_radius': inner_radius_2,
-                'outer_radius': outer_radius_2
-            },
-            mid_params
+            {"vector": self.axis, "inner_radius": inner_radius_2, "outer_radius": outer_radius_2},
+            mid_params,
         )
 
         self.shell = self.operations
