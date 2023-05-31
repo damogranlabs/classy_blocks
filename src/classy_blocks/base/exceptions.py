3c8467db5a61979ad69aa800b1520142fb97044c
schperplata <domen.jurkovic@gmail.com>
2023-05-31 09:47:25 +0200

    Point creation assert -> exception

--- src/classy_blocks/base/exceptions.py
+++ src/classy_blocks/base/exceptions.py
@@ -1,3 +1,6 @@
+from classy_blocks.types import PointType
+
+
 class VertexNotFoundError(Exception):
     """Raised when a vertex at a given point in space doesn't exist yet"""
 
@@ -13,3 +16,15 @@ class CornerPairError(Exception):
 class UndefinedGradingsError(Exception):
     """Raised when the user hasn't supplied enough grading data to
     define all blocks in the mesh"""
+
+
+class ShapeCreationError(Exception):
+    """Base class for shape creation errors (invalid parameters/types to
+    shape constructors)"""
+
+
+class PointCreationError(ShapeCreationError):
+    def __init__(self, msg: str, position: PointType):
+        self.position = position
+
+        super().__init__(f"{msg}\n\tPosition: {position}")
