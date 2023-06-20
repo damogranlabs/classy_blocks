9574bc735a6f258f3b759563faf153ea243bf7bb
nejc <kandelabr@gmail.com>
2023-06-20 23:00:38 +0200

    A Shell example with a 7-block sphere
    There is a bug in project_face(points=True) where point projection is
    not copied into new Vertices. Will fix before merge

--- examples/shape/shell.py
+++ examples/shape/shell.py
@@ -1,20 +1,38 @@
 import os
 
 import classy_blocks as cb
-from classy_blocks.construct.shapes.shell import Shell
 
 mesh = cb.Mesh()
 
-box = cb.Box([0, 0, 0], [1, 1, 1])
+
+# Create a 7-block sphere by offsetting a box's faces.
+box = cb.Box([-0.5, -0.5, -0.5], [0.5, 0.5, 0.5])
 
 for i in range(3):
     box.chop(i, count=10)
 mesh.add(box)
 
+# faces must point 'out' of the original block or
+# newly created blocks will be inside-out;
+offset_faces = [box.get_face(orient) for orient in ("bottom", "top", "left", "right", "front", "back")]
+for i in (0, 2, 4):
+    offset_faces[i].invert()
 
-offset_faces = [box.get_face("top"), box.get_face("right"), box.get_face("front").invert()]
-shell = Shell(offset_faces, 0.2)
+shell = cb.Shell(offset_faces, 0.5)
 shell.chop(count=10)
+
+for operation in shell.operations:
+    operation.project_side("top", "sphere", edges=True, points=True)
+
 mesh.add(shell)
+mesh.add_geometry(
+    {
+        "sphere": [
+            "type searchableSphere",
+            "centre (0 0 0)",
+            "radius 1.5",
+        ]
+    }
+)
 
 mesh.write(os.path.join("..", "case", "system", "blockMeshDict"), debug_path="debug.vtk")
