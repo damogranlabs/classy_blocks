e3f1de716317e184e73d6e261af9f0fbe186ec20
nejc <kandelabr@gmail.com>
2023-07-03 22:15:22 +0200

    Fix: implicit optional radius

--- src/classy_blocks/modify/find/vertex.py
+++ src/classy_blocks/modify/find/vertex.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from classy_blocks.items.vertex import Vertex
 from classy_blocks.mesh import Mesh
@@ -13,7 +13,7 @@ class VertexFinder:
     def __init__(self, mesh: Mesh):
         self.mesh = mesh
 
-    def by_position(self, position: PointType, radius: float = None) -> List[Vertex]:
+    def by_position(self, position: PointType, radius: Optional[float] = None) -> List[Vertex]:
         """Returns a list of vertices that are
         inside a sphere of given radius; if that is not given,
         constants.TOL is taken"""
