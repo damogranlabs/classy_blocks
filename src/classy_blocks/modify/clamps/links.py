a55392b06c1a90c83040c22c7332b6565231c167
nejc <kandelabr@gmail.com>
2023-11-06 12:23:06 +0100

    Add a RotationLink

--- src/classy_blocks/modify/clamps/links.py
+++ src/classy_blocks/modify/clamps/links.py
@@ -1,7 +1,10 @@
 import abc
 
+import numpy as np
+
 from classy_blocks.items.vertex import Vertex
-from classy_blocks.types import NPPointType
+from classy_blocks.types import NPPointType, PointType, VectorType
+from classy_blocks.util import functions as f
 
 
 class LinkBase(abc.ABC):
@@ -35,5 +38,24 @@ class TranslationLink(LinkBase):
         return self.leader.position + self.vector
 
 
-class RotationLink:
-    pass
+class RotationLink(LinkBase):
+    """A link that maintains the same angular displacement
+    between parent clamp/vertex and the linked one,
+    around a given axis.
+
+    It will only work correctly when leader is rotated
+    around given axis and origin."""
+
+    def __init__(self, leader: Vertex, follower: Vertex, origin: PointType, axis: VectorType):
+        super().__init__(leader, follower)
+
+        self.origin = np.array(origin)
+        self.axis = np.array(axis)
+
+        self.orig_leader_pos = np.copy(leader.position)
+        self.orig_follower_pos = np.copy(follower.position)
+
+    def transform(self) -> NPPointType:
+        transform_angle = f.angle_between(self.leader.position, self.orig_leader_pos)
+
+        return f.rotate(self.orig_follower_pos, transform_angle, self.axis, self.origin)
--- tests/test_optimize/test_links.py
+++ tests/test_optimize/test_links.py
@@ -4,20 +4,16 @@ import numpy as np
 
 from classy_blocks.items.vertex import Vertex
 from classy_blocks.modify.clamps.free import FreeClamp
-from classy_blocks.modify.clamps.links import TranslationLink
+from classy_blocks.modify.clamps.links import RotationLink, TranslationLink
 
 
-class LinkTests(unittest.TestCase):
+class TranslationLinkTests(unittest.TestCase):
     def setUp(self):
         self.leader = Vertex([0, 0, 0], 0)
         self.follower = Vertex([1, 1, 1], 1)
 
-    @property
-    def link(self) -> TranslationLink:
-        return TranslationLink(self.leader, self.follower)
-
     def test_translate(self):
-        link = self.link
+        link = TranslationLink(self.leader, self.follower)
 
         self.leader.move_to([3, 3, 3])
         link.update()
@@ -25,10 +21,24 @@ class LinkTests(unittest.TestCase):
         np.testing.assert_equal(self.follower.position, [4, 4, 4])
 
     def test_update_clamp(self):
-        link = self.link
+        link = TranslationLink(self.leader, self.follower)
         clamp = FreeClamp(self.leader)
         clamp.add_link(link)
 
         clamp.update_params([3, 3, 3])
 
         np.testing.assert_equal(self.follower.position, [4, 4, 4])
+
+
+class RotationLinkTests(unittest.TestCase):
+    def setUp(self):
+        self.leader = Vertex([1, 0, 0], 0)
+        self.follower = Vertex([0, 1, 0], 1)
+
+    def test_rotate(self):
+        link = RotationLink(self.leader, self.follower, [0, 0, 0], [0, 0, 1])
+
+        self.leader.move_to([0, 1, 0])
+        link.update()
+
+        np.testing.assert_equal(self.follower.position, [-1, 0, 0])
