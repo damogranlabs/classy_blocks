1455d1aaa2ddf6a72ad51f9247eb1c0174cb4bac
Nejc Jurkovic <kandelabr@gmail.com>
2025-03-14 13:32:54 +0100

    Bump version, update readme/changelog, add tests

---
+++ tests/test_bugs/test_grading.py
@@ -0,0 +1,43 @@
+import unittest
+from typing import get_args
+
+import numpy as np
+
+import classy_blocks as cb
+from classy_blocks.cbtyping import DirectionType
+
+
+class GradingBugTests(unittest.TestCase):
+    def test_invert_grading(self):
+        # Bug case; two blocks with separate bottom faces share the same top face.
+        # Grading in one direction must be inverted
+        #          /|\
+        #        / / \ \
+        #      /  /   \  \
+        #    /   /     \   \
+        #  /____/   ^   \____\
+        #  base  common  neighbour
+
+        box = cb.Box([0, 0, 0], [1, 1, 1])
+
+        base_face = box.bottom_face
+        neighbour_face = base_face.copy().translate([4, 0, 0])
+        common_face = base_face.copy().rotate(np.pi / 2, [0, 1, 0]).translate([2, 0, 2])
+
+        left_loft = cb.Loft(base_face, common_face)
+        right_loft = cb.Loft(neighbour_face, common_face.copy().shift(2).invert())
+
+        for axis in get_args(DirectionType):
+            left_loft.chop(axis, start_size=0.05, total_expansion=5)
+
+        right_loft.chop(2, count=10)
+
+        mesh = cb.Mesh()
+        mesh.add(left_loft)
+        mesh.add(right_loft)
+
+        mesh.assemble()
+        mesh.block_list.assemble()
+
+        self.assertEqual(mesh.blocks[0].format_grading(), "simpleGrading ( 5 5 5 )")
+        self.assertEqual(mesh.blocks[1].format_grading(), "simpleGrading ( 0.2 5 1 )")
